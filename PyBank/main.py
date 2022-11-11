c6c80464f3ba21062a2772cde0e7631fe62490a6
geonwoojeongg <andy.jeong0817@gmail.com>
2022-11-10 22:53:19 -0500

    upload files

---
+++ PyBank/main.py
@@ -0,0 +1,49 @@
+import os
+import csv
+
+#find path
+csvpath = os.path.join("PyBank", "Resources", "budget_data.csv")
+
+# open path
+with open(csvpath, 'r+') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+# skip header row
+    next(csvreader)
+
+    date = []
+    profit_losses = []
+
+    for row in csvreader:
+        date.append(row[0])
+        profit_losses.append(int(row[1]))
+
+# print total months
+    total_months = len(date)
+
+# total amount
+    total_amount = sum(profit_losses)
+
+# average change
+    change = []
+
+    for i in range(total_months-1):
+        change.append(profit_losses[i+1] - profit_losses[i])
+    sum_change = sum(change)
+    average_change = round(sum_change/len(change), 2)
+
+# Greatest increase/decrease in profits
+    greatest_increase = max(change)
+    greatest_decrease = min(change)
+    greatest_increase_index = change.index(max(change))
+    greatest_increase_date = (date[greatest_increase_index+1])
+    greatest_decrease_index = change.index(min(change))
+    greatest_decrease_date = (date[greatest_decrease_index+1])
+
+print(
+f""" Financial Analysis
+----------------------------
+Total months: {total_months}
+Total: ${total_amount}
+Average Change: ${average_change}
+Greatest Increase in Profits: {greatest_increase_date} (${greatest_increase})
+Greatest Decrease in Profits: {greatest_decrease_date} (${greatest_decrease})""")
---
+++ PyPoll/main.py
@@ -0,0 +1,62 @@
+import os
+import csv
+
+#find path
+csvpath = os.path.join("PyPoll", "Resources", "election_data.csv")
+
+# open path
+with open(csvpath, 'r+') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+# skip header row
+    next(csvreader)
+
+    ballotID = []
+    candidates = []
+    candidate_list = []
+
+    for row in csvreader:
+        ballotID.append(row[0])
+        candidates.append(str(row[2]))
+
+# total votes
+    total_votes = len(ballotID)
+    print(total_votes)
+
+# total list of candidates
+    for i in candidates:
+        if i not in candidate_list:
+            candidate_list.append(i)
+    
+    print(candidate_list)
+
+# percent
+    candidate_votes = {}
+    percentage = []
+    for name in candidates:
+        if name not in candidate_votes:
+            candidate_votes[name]= 1
+        else: 
+            candidate_votes[name] += 1
+    
+    print(candidate_votes)
+
+    def find_percentage(candidate_votes, name, total_ballots):
+        return round(candidate_votes[name]/total_ballots * 100,3)
+
+    for name in candidate_votes:
+        percentage.append(find_percentage(candidate_votes, name, total_votes))
+    
+    print_names = list(candidate_votes.keys())
+    print_votes = list(candidate_votes.values())
+
+    print(f"""
+Election Results
+---------------------------
+Total Votes: {total_votes}
+---------------------------
+{print_names[0]}: {percentage[0]}% ({print_votes[0]})
+{print_names[1]}: {percentage[1]}% ({print_votes[1]})
+{print_names[2]}: {percentage[2]}% ({print_votes[2]})
+----------------------------
+Winner: {max(candidate_votes, key=candidate_votes.get)}
+----------------------------""")
